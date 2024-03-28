2a6cf4001cc5e71b3c9ebf3fc6d427d8d04aa529
ngducduyyy <ngducduyyy@gmail.com>
2024-03-28 23:12:50 +0700

    new

--- src/components/NewRelease.jsx
+++ src/components/NewRelease.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import data from "../data.json";
 import Movie from './Movie';
 const NewRelease = () => {
-    const [release, setRelease] = useState([]);
   return (
     <div className='release'>
 <Movie key={data.id}/>
